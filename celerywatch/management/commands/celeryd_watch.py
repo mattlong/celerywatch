b4bd0a94572d958a868b1ad154dbc0663d1a2c4b
Matt Long <matt@crocodoc.com>
2011-11-14 01:33:41 +0000

    options wrangling

--- celerywatch/management/commands/celeryd_watch.py
+++ celerywatch/management/commands/celeryd_watch.py
@@ -9,14 +9,12 @@ class Command(BaseCommand):
     help = 'Runs a celeryd watcher daemon that will shutdown celeryd if it fails too many tasks.'
     requires_model_validation = False
     option_list = BaseCommand.option_list + (
-    make_option('-g', '--procgrep', action='store', type='string', dest='proc_grep', default=None, help='TODO'),
+    make_option('-g', '--procgrep', action='store', type='string', dest='process_grep', default=None, help='TODO'),
     make_option('-t', '--stopthreshold', action='store', type='float', dest='stop_threshold', default=0.5, help='TODO'),
     make_option('-m', '--mintasks', action='store', type='int', dest='min_tasks', default=10, help='TODO'),
+    make_option('-p', '--poststopscript', action='store', type='string', dest='post_stop_script', default=None, help='TODO'),
+    make_option('-o', '--override', action='store_true', dest='override_stop_script', default=False, help='TODO'),
     )
 
     def handle(self, *args, **options):
-        watcher.evdump(
-                stop_threshold=options['stop_threshold'],
-                min_tasks=options['min_tasks'],
-                process_grep=options['proc_grep'],
-        )
+        watcher.evdump(*args, **options)
--- celerywatch/watcher.py
+++ celerywatch/watcher.py
@@ -1,5 +1,5 @@
 import sys, subprocess
-from pkg_resources import resource_exists, resource_filename
+from pkg_resources import resource_filename
 
 from celery.app import app_or_default
 from celery.datastructures import LRUCache
@@ -8,12 +8,23 @@ TASK_HISTORY = LRUCache(limit=10)
 
 class PDFWorkerMonitor(object):
 
-    def __init__(self, stop_threshold=0.1, min_tasks=1, process_grep=None):
+    def __init__(self,
+            stop_threshold=0.1,
+            min_tasks=1,
+            process_grep=None,
+            post_stop_script=None,
+            override_stop_script=False,
+        ):
         self.MIN_TASKS = min_tasks
         self.STOP_THRESHOLD = stop_threshold
+        self.PROCESS_GREP = process_grep
+        self.POST_STOP_SCRIPT=post_stop_script
+        self.OVERRIDE_STOP_SCRIPT=override_stop_script
+
+        #print self.MIN_TASKS, self.STOP_THRESHOLD, self.PROCESS_GREP, self.POST_STOP_SCRIPT, self.OVERRIDE_STOP_SCRIPT
+
         self.task_count = 0
         self.error_rate = 0.0
-        self.PROCESS_GREP = process_grep
 
     def kill_celery(self, non_daemon=False):
         #cmds = ['timeout', '-k', '1s', '-s', 'SIGTERM', '60s', '/etc/init.d/celeryd', 'stop']
@@ -50,9 +61,21 @@ class PDFWorkerMonitor(object):
         self.task_count += 1
         TASK_HISTORY[event['uuid']] = 'success'
 
-def evdump(stop_threshold=0.5, min_tasks=10, process_grep=None, app=None):
-    app = app_or_default(app)
-    monitor = PDFWorkerMonitor(stop_threshold=stop_threshold, min_tasks=min_tasks, process_grep=process_grep)
+#def evdump(stop_threshold=0.5, min_tasks=10, process_grep=None):
+def evdump(*args, **kwargs):
+    stop_threshold = kwargs.get('stop_threshold', 0.5)
+    min_tasks = kwargs.get('min_tasks', 10)
+    process_grep = kwargs.get('process_grep', None)
+    post_stop_script = kwargs.get('post_stop_script', None)
+    override_stop_script = kwargs.get('override_stop_script', False)
+    app = app_or_default(None)
+    monitor = PDFWorkerMonitor(
+            stop_threshold=stop_threshold,
+            min_tasks=min_tasks,
+            process_grep=process_grep,
+            post_stop_script=post_stop_script,
+            override_stop_script=override_stop_script,
+    )
     conn = app.broker_connection()
     #recv = app.events.Receiver(conn, handlers={"*": dumper.on_event})
     recv = app.events.Receiver(conn, handlers={'task-succeeded': monitor.on_task_succeeded, 'task-failed': monitor.on_task_failed})
@@ -63,4 +86,4 @@ def evdump(stop_threshold=0.5, min_tasks=10, process_grep=None, app=None):
         conn and conn.close()
 
 if __name__ == "__main__":
-    evdump()
+    evdump(*sys.argv)
